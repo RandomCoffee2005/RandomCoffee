660c994bb9c0a2ae85d6d10bb3cc543ee3a7e00b
Timur Usmanov <t.usmanov@innopolis.university>
2026-03-31 16:58:43 +0300

    Add tests for db.initialize_if_not_exists

---
+++ randomcoffee/tests/test_db.py
@@ -0,0 +1,25 @@
+import db
+import os
+from pytest_mock import MockerFixture
+import pytest
+
+
+def test_newdb(mocker: MockerFixture):
+    dbpath = "/tmp/db.bin"
+    if os.path.exists(dbpath):
+        os.remove(dbpath)
+    _ = mocker.patch('envconfig.config.dbpath', dbpath);
+    db.initialize_if_not_exists()
+    assert os.path.exists(dbpath)
+    with db.connect(readonly=True) as conn:
+        for table in 'users', 'user_interests', 'pairings':
+            cur = conn.execute(f"select * from {table}")
+            assert len(cur.fetchall()) == 0
+            cur.close()
+
+    with db.connect(readonly=True) as conn:
+        with pytest.raises(Exception):
+            cur = conn.execute("""insert into users values ('alice@qweqksdm', 'alice', 'dksmclksdmclksdmcl')""")
+            cur.close()
+
+    os.remove(dbpath)
